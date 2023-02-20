5455e8f5cc82bb6f7003262c606187526b4a63c2
EduardRo <eduardstancu@yahoo.com>
2023-02-20 22:04:19 +0200

    modif200223

---
+++ lib/models/questions.dart
@@ -0,0 +1,41 @@
+class Question {
+  final int? id;
+  final int? answerindex;
+  final String? question;
+  final List<String>? options;
+
+  Question({
+    this.id,
+    this.question,
+    this.options,
+    this.answerindex,
+  });
+}
+
+const List sampledata = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answerindex": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answerindex": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answerindex": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answerindex": 2,
+  },
+];
