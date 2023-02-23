46e6b4708b515ca32f822929295b8913ea77738c
EduardRo <eduardstancu@yahoo.com>
2023-02-23 17:29:53 +0200

    add data

--- lib/models/questions.dart
+++ lib/models/questions.dart
@@ -38,4 +38,16 @@ const List sampledata = [
     "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
     "answerindex": 2,
   },
+  {
+    "id": 5,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answerindex": 2,
+  },
+  {
+    "id": 6,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answerindex": 2,
+  },
 ];
