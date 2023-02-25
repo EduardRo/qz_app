ea1f222cfabe2fb0aacf5f7e3dc71d85f3081ca4
EduardRo <eduardstancu@yahoo.com>
2023-02-25 18:10:36 +0200

    scf tst

---
+++ lib/_testing/scaffoldtst.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title = 'My title';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My AppBar'),
+      ),
+      body: const Center(
+        child: Text('this is the center body'),
+      ),
+    );
+  }
+}
